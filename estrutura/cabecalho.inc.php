cea3b687a14efae00404762dff6e325d757cf4bd
Jackson Meires <lordjackson@gmail.com>
2023-04-25 17:30:19 -0300

    aula de formulário com PHP

---
+++ estrutura/cabecalho.inc.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+<header>
+<h1>Cinema On-line</h1>
+
+</header>
---
+++ estrutura/menu.inc.php
@@ -0,0 +1,10 @@
+<nav>
+<h2>Menu</h2>
+<ul>
+<li><a href="index.php">Principal</a></li>
+<li><a href="destaques.php">Destaques</a></li>
+<li><a href="programacao.php">Programacao</a></li>
+<li><a href="fotos.php">Fotos</a></li>
+<li><a href="paginas/contato.php">Contato</a></li>
+</ul>
+</nav>
